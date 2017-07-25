3aaf3c0cc1fbbeb94b714be4f82eb8ea4649e8e8
ahmetomerv <ahmetomerv@gmail.com>
2017-07-26 02:55:36 +0300

    added reveal js animations

--- js/main.js
+++ js/main.js
@@ -1,3 +1,60 @@
 // smooth scroll
 $('#nav_scroll').localScroll();
 $('.wrapper').localScroll();
+
+
+// scroll reveal
+window.sr = ScrollReveal();
+sr.reveal('header', {
+  duration: 2000,
+  origin: 'top'
+});
+
+sr.reveal('.web-dev', {
+  duration: 2000,
+  origin: 'left',
+  distance: '300px'
+});
+
+sr.reveal('.web-des', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '300px'
+});
+
+sr.reveal('.psd', {
+  duration: 2000,
+  origin: 'right',
+  distance: '300px'
+});
+
+
+sr.reveal('.res-des', {
+  duration: 2000,
+  origin: 'left',
+  distance: '300px'
+});
+
+sr.reveal('.ui-ux', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '300px'
+});
+
+sr.reveal('.e-com', {
+  duration: 2000,
+  origin: 'right',
+  distance: '300px'
+});
+
+sr.reveal('#what-I-do', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '50px'
+});
+
+sr.reveal('#title-about', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '50px'
+});
