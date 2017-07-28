6af348a50d1b881583d1008be009d9254ebdac6e
ahmetomerv <ahmetomerv@gmail.com>
2017-07-29 01:16:37 +0300

    changed reveal animation duration

--- js/main.js
+++ js/main.js
@@ -11,50 +11,50 @@ sr.reveal('header', {
 });
 
 sr.reveal('.web-dev', {
-  duration: 2000,
+  duration: 1500,
   origin: 'left',
   distance: '300px'
 });
 
 sr.reveal('.web-des', {
-  duration: 2000,
+  duration: 1500,
   origin: 'bottom',
   distance: '300px'
 });
 
 sr.reveal('.psd', {
-  duration: 2000,
+  duration: 1500,
   origin: 'right',
   distance: '300px'
 });
 
 
 sr.reveal('.res-des', {
-  duration: 2000,
+  duration: 1500,
   origin: 'left',
   distance: '300px'
 });
 
 sr.reveal('.ui-ux', {
-  duration: 2000,
+  duration: 1500,
   origin: 'bottom',
   distance: '300px'
 });
 
 sr.reveal('.e-com', {
-  duration: 2000,
+  duration: 1500,
   origin: 'right',
   distance: '300px'
 });
 
 sr.reveal('#what-I-do', {
-  duration: 2000,
+  duration: 1000,
   origin: 'bottom',
   distance: '50px'
 });
 
 sr.reveal('#button-contact', {
-  duration: 2000,
+  duration: 1000,
   origin: 'top',
   distance: '50px'
 });
