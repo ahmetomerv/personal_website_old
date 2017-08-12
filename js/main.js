377ac921fc9ad3b9a692353e51488b3b5f2e8324
ahmetomerv <ahmetomerv@gmail.com>
2017-08-13 01:23:57 +0300

    added new section - resume. Styles and more

--- js/main.js
+++ js/main.js
@@ -71,27 +71,27 @@ sr.reveal('.tool', {
   distance: '100px'
 });
 
-/* -----  slider  -----*/
-var i = 0; // start point
-var images = [];
-var time = 3000;
-
-// image list
-images[0] = 'img/bleemen-home.jpg'
-images[1] = 'img/bleemen-contact.jpg';
-images[2] = 'img/talent2celeb-home.jpg';
-
-// change image
-function changeImg () {
-  document.slide.src = images[i];
-
-  if (i < images.length - 1) {
-    i++;
-  } else {
-    i = 0;
-  }
-
-  setTimeout('changeImg()', time);
-}
-
-window.onload = changeImg;
+// /* -----  slider  -----*/
+// var i = 0; // start point
+// var images = [];
+// var time = 3000;
+//
+// // image list
+// images[0] = 'img/bleemen-home.jpg'
+// images[1] = 'img/bleemen-contact.jpg';
+// images[2] = 'img/talent2celeb-home.jpg';
+//
+// // change image
+// function changeImg () {
+//   document.slide.src = images[i];
+//
+//   if (i < images.length - 1) {
+//     i++;
+//   } else {
+//     i = 0;
+//   }
+//
+//   setTimeout('changeImg()', time);
+// }
+//
+// window.onload = changeImg;
