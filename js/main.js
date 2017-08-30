12f973960f25e97e6389a3ff4877b43ac1502bf4
ahmetomerv <ahmetomerv@gmail.com>
2017-08-30 23:34:32 +0300

    add loader - style script markup

--- js/main.js
+++ js/main.js
@@ -60,28 +60,8 @@ sr.reveal('.logo-container', {
 });
 
 
-
-// /* -----  slider  -----*/
-// var i = 0; // start point
-// var images = [];
-// var time = 3000;
-//
-// // image list
-// images[0] = 'img/bleemen-home.jpg'
-// images[1] = 'img/bleemen-contact.jpg';
-// images[2] = 'img/talent2celeb-home.jpg';
-//
-// // change image
-// function changeImg () {
-//   document.slide.src = images[i];
-//
-//   if (i < images.length - 1) {
-//     i++;
-//   } else {
-//     i = 0;
-//   }
-//
-//   setTimeout('changeImg()', time);
-// }
-//
-// window.onload = changeImg;
+// loader
+var loader = document.getElementById('loader');
+window.addEventListener('load', function() {
+  loader.style.display = 'none';
+})
